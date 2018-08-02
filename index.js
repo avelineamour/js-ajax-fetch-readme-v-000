2255ef9bba2cacb22af4b06f3f5050305f7075dc
Razelle McCarrick <razelle@satyaorthopedic.com>
2018-08-02 18:31:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much.";
+
+const token = '95eb2cfee828c02f51ada759e9db051fccbcf4b3'
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
